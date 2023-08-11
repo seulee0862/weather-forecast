a5f7c24e654ad079acd6b6098a3279d262404f50
lsh <seulee0862@gmail.com>
2023-08-11 12:32:58 +0900

    first commit

---
+++ src/main/java/com/project02server/Project02ServerApplication.java
@@ -0,0 +1,13 @@
+package com.project02server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project02ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project02ServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project02server/Project02ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project02server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project02ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
