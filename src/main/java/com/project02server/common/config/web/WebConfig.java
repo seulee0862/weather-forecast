b37e77dd618cad5870c0ee40ca79f0ef97580b95
lsh <seulee0862@gmail.com>
2023-09-07 17:07:05 +0900

    Refactor: CorsConfig Allow Origin http로 변경

--- src/main/java/com/project02server/common/config/web/WebConfig.java
+++ src/main/java/com/project02server/common/config/web/WebConfig.java
@@ -7,14 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	private final String FRONTEND = "http://localhost:3005";
+	private final String[] FRONTENDS = {"https://3.34.18.190:3005", "https://namomdaero.site"};
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins(FRONTEND)
+			.allowedOrigins(FRONTENDS)
 			.allowedMethods("GET", "POST","DELETE","PUT","PATCH")
 			.maxAge(3000);
 	}
 }
-
