78c160050bce724914d8f3e0e7b3f8943e599d54
lsh <seulee0862@gmail.com>
2023-11-29 19:08:31 +0900

    feat: add kakao login

--- src/main/java/com/project02server/common/config/web/WebConfig.java
+++ src/main/java/com/project02server/common/config/web/WebConfig.java
@@ -2,7 +2,10 @@ package com.project02server.common.config.web;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.project02server.common.config.web.interceptor.JwtInterceptor;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
--- src/main/java/com/project02server/user/service/UserService.java
+++ src/main/java/com/project02server/user/service/UserService.java
@@ -26,11 +26,38 @@ public class UserService {
 	}
 
 	private User save(String email, String oAuthProvider) {
-		return userRepository.save(User.of(email, oAuthProvider));
+		Subscribe subscribe = subscribeService.createDefaultSubscription();
+		return userRepository.save(User.of(email, oAuthProvider, subscribe));
+	}
+
+	public User findById(Long id) {
+		return userRepository.findById(id)
+			.orElseThrow(() -> new BusinessException(ErrorCode.NOT_FOUND_ENTITY));
 	}
 
 	private Optional<User> getByEmailAndPlatform(String email, String oAuthProvider) {
 		return qUserRepository.findByEmailAndPlatform(email, oAuthProvider);
 	}
 
+	@Transactional
+	public void subscribeToWeather(Long userId, SubscribeReqeust reqeust) {
+		User user = findById(userId);
+
+		if (!coordinateService.existByRegionName(reqeust.getRegionName())) {
+			throw new BusinessException(ErrorCode.INVALID_REGION_NAME);
+		}
+
+		user.getSubscribe().activeSubscription(reqeust.getRegionName());
+	}
+
+	@Transactional
+	public void unsubscribeFromWeather(Long userId) {
+		User user = findById(userId);
+
+		user.getSubscribe().deactivateSubscription();
+	}
+
+	public List<User> findUsersByActiveSubscribe() {
+		return userRepository.findUsersByActiveSubscribe();
+	}
 }
